763f430ca1498282a94d3319e63dfaf8f64c2724
huaphucdung <huaphucdung2001@gmail.com>
2022-04-22 16:22:13 +0700

    Làm xong socket hiển thị trạng thái đăng nhập và đăng xuất

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 var socket = io();
 
-socket.on('message', msg=> {
-    console.log(msg)
+var account = {
+    username : $('#user').html(),
+    email: $('#user').data('email')
+}
+
+//Join server 
+socket.emit('online', account)
+
+socket.on('login', users=> {
+    const keys = Object.keys(users)
+    console.log(keys)
+    $('.status').removeClass('online').addClass('offline')
+    $('#user-status').removeClass('offline').addClass('online')
+    keys.forEach(i => {
+        var element = document.getElementById(i)
+        $(element).removeClass('offline')
+        $(element).addClass('online')
+    })
 })
 
--- routes/chat.js
+++ routes/chat.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const async = require('hbs/lib/async');
 var router = express.Router();
-var getRoom = require('../lib/getRoom.js')
+var getRoom = require('../lib/Room.js')
 /* GET chat */
 router.get('/',async function(req, res) {
   if(!req.session.account)
@@ -27,11 +27,13 @@ router.get('/r/:id',async function(req, res) {
   var accountInfo = req.session.account
   var css = ['/css/chat.css','/css/normalize.css']
   var listRooms = await getRoom.listRoom(accountInfo.email)
-  
+  var roomChat = await getRoom.getRoom(req.params.id,accountInfo.email)
+
   var content = { title: 'Chat',
    cssFile : css,
    account : accountInfo,
-   friends: listRooms
+   friends: listRooms,
+   room: roomChat.name.name
    }
   res.render('index', content);
 });
